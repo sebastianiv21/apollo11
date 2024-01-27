f8d299d5743a33e78e8ef5ab23db90fa31c13a81
sebastianiv21 <seluti98@gmail.com>
2024-01-26 21:21:44 -0500

    wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,35 +6,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Apollo-11'
-copyright = '2023, LLA'
-author = 'LLA'
-release = '1.0'
+project = "Apollo-11"
+copyright = "2023, LLA"
+author = "LLA"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-language = 'es'
+language = "es"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = ['custom.css']
+# html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.duration',
-    
+    "sphinx.ext.duration",
 ]
-
-
-
